a9737ed4e8bee38f0fc4eced1e875292f2acd5c0
Mercy Oroo <mercy.oroo@student.moringaschool.com>
2024-03-25 11:33:33 +0300

    js lab commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+ function destructivelyAppendCat (name) {
+    return cats.push ("Ralph")
+ }
+
+ function destructivelyPrependCat (name) {
+    return cats.unshift ("Bob")
+ }
+ function destructivelyRemoveLastCat (name) {
+    return cats.pop ()
+ }
+ function destructivelyRemoveFirstCat (name) {
+    return cats.shift()
+ }
+//  function appendCat(name) {
+//     return cats.splice(0,"Arnold")
+//  }
+ function appendCat (name) {
+    return [...cats,name]
+ }  
+ function prependCat (name) {
+    return [name,...cats]
+ }
+ function removeLastCat(name) {
+    return cats.slice (0,cats.length-1)
+ }
+ function removeFirstCat(name) {
+    return cats.slice (1)
+ }
